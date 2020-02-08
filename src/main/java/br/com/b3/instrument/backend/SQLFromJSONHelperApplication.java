cbd72322fd54de4ed3161ad9459e78335ee0df9b
Diego <diego.guisosi@gmail.com>
2020-02-08 14:30:03 -0300

    implementacao de helper para conversao de json para sql

---
+++ src/main/java/br/com/b3/instrument/backend/SQLFromJSONHelperApplication.java
@@ -0,0 +1,52 @@
+package br.com.b3.instrument.backend;
+
+import br.com.b3.instrument.backend.domain.HistoricalPrice;
+import br.com.b3.instrument.backend.domain.Instrument;
+import br.com.b3.instrument.backend.util.JSONUtil;
+import br.com.b3.instrument.backend.util.ResourceUtil;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class SQLFromJSONHelperApplication {
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        List<Instrument> instruments = extractEntities("instruments.json", Instrument.class);
+        print(instruments,
+                instrument -> String.format("INSERT INTO tbl_instr (instr_id, instr_symbol, instr_mat_date) " +
+                                "VALUES (%d, '%s', parsedatetime('%s', 'yyyy-MM-dd'));",
+                        instrument.getId(), instrument.getSymbol(), instrument.getMaturityDate().format(DateTimeFormatter.ISO_LOCAL_DATE)));
+
+        System.out.println();
+        System.out.println();
+
+        Map<String, Long> instrumentIdBySymbol = instruments.stream().collect(Collectors.toMap(Instrument::getSymbol, Instrument::getId));
+        List<HistoricalPrice> historicalPrices = extractEntities("prices.json", HistoricalPrice.class);
+        print(historicalPrices, price -> {
+            Long instrumentId = instrumentIdBySymbol.get(price.getSymbol());
+            return Arrays.stream(price.getSeries())
+                    .map(serie -> String.format("INSERT INTO tbl_hist_price (instr_id, hist_price_date, hist_price_value) " +
+                                    "VALUES (%d, parsedatetime('%s', 'yyyy-MM-dd'), %.0f);",
+                            instrumentId, serie.getDate().format(DateTimeFormatter.ISO_LOCAL_DATE), serie.getPrice()))
+                    .collect(Collectors.joining("\n"));
+        });
+    }
+
+    private static <T> List<T> extractEntities(String resourceName, Class<T> resourceType) throws IOException, URISyntaxException {
+        String jsonString = ResourceUtil.getContentAsString(resourceName);
+        return JSONUtil.parseAsList(jsonString, resourceType);
+    }
+
+    private static <T> void print(List<T> entities, Function<T, String> converterFunction) {
+        entities.stream()
+                .map(converterFunction)
+                .forEach(System.out::println);
+    }
+
+}
